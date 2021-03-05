35f83948ff8aa4cbe949ed8f1070ed1f25fd1e5a
LuisMSoares <dudu.sg32@yahoo.com.br>
2021-03-05 17:42:56 -0300

    hotfix: login on interative docs

--- app/core/auth.py
+++ app/core/auth.py
@@ -1,6 +1,7 @@
 # Standard Imports
 from fastapi import Depends
 from fastapi import APIRouter
+from fastapi.security import OAuth2PasswordRequestForm
 from fastapi_login import LoginManager
 from fastapi_login.exceptions import InvalidCredentialsException
 from sqlalchemy import or_
@@ -18,45 +19,49 @@ from ..db.engine import get_db
 from ..modules.users.models import User
 
 
-manager = LoginManager(JWT_SECRET, tokenUrl=API_PREFIX+'/login')
+manager = LoginManager(JWT_SECRET, tokenUrl=API_PREFIX+'/auth/token')
 route = APIRouter()
 
 
 # Login Schema
 class LoginData(BaseSchema):
-    login: str
+    username: str
     password: str
 
     class Config:
         schema_extra = {
             "example": {
-                "login": "dudu@mail.com",
+                "username": "dudu@mail.com",
                 "password": "mysecretpassword"
             }
         }
 
 @manager.user_loader
-def load_user(login: str):
+def load_user(username: str):
     db = next(get_db())
     user = db.query(User).filter(or_(
-        User.email == login,
-        User.username == login
+        User.email == username,
+        User.username == username
     )).first()
     return user
 
+@route.post('/auth/token', include_in_schema=False)
+def auth_token(data: OAuth2PasswordRequestForm = Depends()):
+    return login(data)
+
 @route.post('/login')
 def login(data: LoginData):
-    login = data.login
+    username = data.username
     password = data.password
 
-    user: User = load_user(login)
+    user: User = load_user(username)
     if not user:
         raise InvalidCredentialsException
     elif user.verify_password(password) == False:
         raise InvalidCredentialsException
 
     access_token = manager.create_access_token(
-        data=dict(sub=login),
+        data=dict(sub=username),
         expires=timedelta(days=JWT_EXPIRATION_DAYS)
     )
     return {'access_token': access_token, 'token_type': 'bearer'}
